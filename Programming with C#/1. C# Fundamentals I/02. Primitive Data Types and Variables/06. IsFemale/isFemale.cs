99339d833859efb9ebd01483a377ffd5cecbf9a9
VladislavIvanov <ivanov.vladdy@gmail.com>
2014-03-07 20:52:31 +0200

    Update

---
+++ Programming with C#/1. C# Fundamentals I/02. Primitive Data Types and Variables/06. IsFemale/isFemale.cs
@@ -0,0 +1,28 @@
+﻿/*
+* 6. Declare a boolean variable called isFemale and assign an
+* appropriate value corresponding to your gender.
+*/
+
+using System;
+ 
+class IsFemale
+{
+    static void Main()
+    {
+        Console.Write("Enter your gender (Male/Female): ");
+        string gender = Console.ReadLine();
+
+        switch (gender.ToLower())
+        {
+            case "female":
+                Console.WriteLine("Yes, you are female!");
+                break;
+            case "male":
+                Console.WriteLine("No, you are male!");
+                break;
+            default:
+                Console.WriteLine("Incorrect input data!");
+                break;
+        }
+    }
+}
